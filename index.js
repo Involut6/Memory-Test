97c4e173778db2a410b4c0e86ff515f4e15756ec
Abdulqoyum Aliyu <94830568+Involut6@users.noreply.github.com>
2022-02-02 18:38:50 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,90 @@
+var userClickedPattern = [];
+
+var gamePattern = [];
+
+var buttonColours = ["red", "blue", "green", "yellow"];
+
+var level = 0;
+
+started = false;
+
+$(document).keydown(function() {
+  if (!started) {
+    $("h1").text("Level " + level);
+    nextSequence();
+    started = true;
+  }
+})
+
+$(".btn").click(function() {
+  var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+
+  playSound(userChosenColour);
+  checkAnswer(userClickedPattern.length - 1);
+
+});
+
+function nextSequence() {
+
+  userClickedPattern = [];
+
+  level++;
+
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+  $("h1").text("Level " + level);
+
+  $("#" + randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+
+  playSound(randomChosenColour);
+}
+
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+
+function animatePress() {
+
+  $(".btn").click(function() {
+    $(this).addClass("pressed");
+    setTimeout(function() {
+      $(".btn").removeClass("pressed")
+    }, 100);
+  });
+}
+
+animatePress();
+
+function checkAnswer(currentLevel) {
+
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+    if (gamePattern.length === userClickedPattern.length) {
+      setTimeout(function() {
+        nextSequence();
+      }, 1000);
+    }
+
+  } else {
+    var audio = new Audio("sounds/wrong.mp3");
+    audio.play();
+    $("body").addClass("game-over");
+    setTimeout(function() {
+      $("body").removeClass("game-over");
+    }, 200);
+    $("h1").text("Game Over, Press any key to Restart.")
+    startOver();
+  }
+}
+
+function startOver() {
+  level = 0;
+  gamePattern = [];
+  started = false;
+}
+
+$(document).click(function() {
+  $(".instructions").remove();
+})
